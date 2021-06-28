836e505718d602c8933be99a0ac80fec62bde35d
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2021-06-28 01:41:34 -0300

    Completed exercise 5.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
-import Login from './components/Login'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,22 +15,71 @@ const App = () => {
     )  
   }, [])
 
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        <Login setUser={setUser} />
-      </div>
-    )
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogListUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
+  const login = async (username, password) => {
+    try {
+      const user = await loginService.login(username, password)
+
+      if (user.status === 200) {
+        setUser(user.data)
+      }
+
+      window.localStorage.setItem('loggedBlogListUser', JSON.stringify(user.data))
+    } catch (error) {
+      console.error(error)
+      if (error.response.status === 401) {
+        alert('Wrong username or password')
+      }
+    }
   }
 
-  return (
+  const logout = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogListUser')
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={event => { event.preventDefault(); login(username, password) }} >
+        username
+        <input
+          type="test"
+          value={username}
+          onChange={event => setUsername(event.target.value)} />
+        <br />
+        password
+        <input
+          type="password"
+          value={password}
+          onChange={event => setPassword(event.target.value)} />
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
+  )
+
+  const blogForm = () => (
     <div>
       <h2>blogs</h2>
+      <div>{user.name} logged in<button onClick={logout}>logout</button></div><br />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
+  )
+
+  return (
+    <>
+      { user === null ? loginForm() : blogForm() }
+    </>
   )
 }
 
--- src/services/login.js
+++ src/services/login.js
@@ -5,8 +5,6 @@ const baseUrl = 'http://localhost:3003/api/login'
 const login = (username, password) =>
   axios.post(baseUrl, { username: username, password: password })
 
-const exportDefault = {
+export default {
   login
 }
-
-export default exportDefault
