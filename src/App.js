f0ddc4cf4c4a532cfe16d68a747895390f765823
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2021-06-29 18:39:22 -0300

    Completed exercise 5.4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react'
+
+import './index.css'
+
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -8,6 +12,7 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [notification, setNotification] = useState(null)
   const [blogTitle, setBlogTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [url, setUrl] = useState('')
@@ -23,6 +28,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -41,7 +47,9 @@ const App = () => {
     } catch (error) {
       console.error(error)
       if (error.response && error.response.status === 401) {
-        alert('Wrong username or password')
+        notify({text: 'Wrong username or password', error: true})
+      } else {
+        notify({text: 'Cannot login: server might not be running', error: true})
       }
     }
   }
@@ -51,17 +59,31 @@ const App = () => {
     window.localStorage.removeItem('loggedBlogListUser')
   }
 
-  const createBlog = () => {
-    const response = blogService.create({
-      title: blogTitle,
-      author: author,
-      url: url
-    })
+  const createBlog = async () => {
+    try {
+      const response = await blogService.create({
+        title: blogTitle,
+        author: author,
+        url: url
+      })
+
+      notify({text: `a new blog ${blogTitle} by ${author} created`})
+    } catch (error) {
+      notify({text: 'error adding blog', error: true})
+    }
+  }
+
+  const notify = (message) => {
+    setNotification(message)
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
   }
 
   const loginForm = () => (
-    <div>
+    <>
       <h2>Log in to application</h2>
+      <Notification notification={notification} />
       <form onSubmit={ event => {
         event.preventDefault()
         login(username, password)
@@ -80,12 +102,13 @@ const App = () => {
         <br />
         <input type="submit" value="login" />
       </form>
-    </div>
+    </>
   )
 
   const blogForm = () => (
-    <div>
+    <>
       <h2>blogs</h2>
+      <Notification notification={notification} />
       <div>{user.name} logged in<button onClick={logout}>logout</button></div><br />
       <h2>create new</h2>
       <form onSubmit={ event => {
@@ -112,7 +135,7 @@ const App = () => {
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
-    </div>
+    </>
   )
 
   return (
