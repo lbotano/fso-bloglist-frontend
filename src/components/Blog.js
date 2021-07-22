25738049a2ca5892038eb5f43796f6b2f67c1c22
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2021-07-22 20:06:57 -0300

    Completed exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,33 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [show, setShow] = useState(false)
+  
+  const showStyle = { display: show ? '' : 'none' }
+  const boxStyle = {
+    border: '2px solid #000',
+    margin: '5px 0',
+    padding: '2px'
+  }
+  const buttonStyle = { marginLeft: 5 }
+
+  const toggleShow = () => {
+    setShow(!show)
+  }
+
+
+  return (
+    <div style={boxStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleShow} style={buttonStyle}>
+        {show ? 'hide' : 'view'}
+      </button>
+      <div style={showStyle}>
+        {blog.url}<br />
+        likes {blog.likes} <button>like</button><br />
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
